18449dc8568ff1df8c696312ac82e727bccd290c
kamoto <takahiro.08039@gmail.com>
2022-09-17 14:49:33 +0900

    ヘッダーの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Angular Material
+import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Swiper
 import { SwiperModule } from 'swiper/angular';
@@ -16,20 +18,24 @@ import { SwiperModule } from 'swiper/angular';
 // Component
 import { SwiperComponent } from './component/parts/swiper/swiper.component';
 import { PickButtonComponent } from './component/parts/button/pick-button/pick-button.component';
+import { HeaderComponent } from './component/parts/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwiperComponent,
     PickButtonComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatIconModule,
     MatSliderModule,
     MatButtonModule,
     MatChipsModule,
+    MatToolbarModule,
     SwiperModule,
   ],
   providers: [],
