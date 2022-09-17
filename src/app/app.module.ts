3bc85c6f23bfe44f8f2b70dd3e81912cb7285f36
kamoto <takahiro.08039@gmail.com>
2022-09-18 01:01:02 +0900

    フッターヘッダーなどの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 // Swiper
 import { SwiperModule } from 'swiper/angular';
@@ -19,6 +22,8 @@ import { SwiperModule } from 'swiper/angular';
 import { SwiperComponent } from './component/parts/swiper/swiper.component';
 import { PickButtonComponent } from './component/parts/button/pick-button/pick-button.component';
 import { HeaderComponent } from './component/parts/header/header.component';
+import { SideBarComponent } from './component/parts/side-bar/side-bar.component';
+import { FooterComponent } from './component/parts/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +31,8 @@ import { HeaderComponent } from './component/parts/header/header.component';
     SwiperComponent,
     PickButtonComponent,
     HeaderComponent,
+    SideBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +43,9 @@ import { HeaderComponent } from './component/parts/header/header.component';
     MatButtonModule,
     MatChipsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
     SwiperModule,
   ],
   providers: [],
