5081da3a0db2c2ecd2f160f0c3093c782ae7b1f8
kamoto <takahiro.08039@gmail.com>
2022-11-26 19:42:37 +0900

    フッター下線の実装

--- src/app/component/parts/footer/footer.component.ts
+++ src/app/component/parts/footer/footer.component.ts
@@ -8,11 +8,17 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
 
+  public topFlg: boolean = false;
+  public yoteiList: boolean = false;
+  public categoryListFlg: boolean = false;
+  public requestFlg: boolean = false;
+
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.iconFieldUnderLine();
   }
 
   goTop() {
@@ -29,6 +35,30 @@ export class FooterComponent implements OnInit {
 
   goRequest() {
     this.router.navigate(['request']);
+  }
+
+  /**
+   * アイコンフィールドに下線をつけるか判定
+   */
+  public iconFieldUnderLine() {
+    // 現在URLを取得する
+    const currentUrl = this.router.url;
+
+    if (currentUrl === '/') {
+      this.topFlg = true;
+    }
+
+    if (currentUrl === '/yoteiList') {
+      this.yoteiList = true;
+    }
+
+    if (currentUrl === '/categoryList') {
+      this.categoryListFlg  = true;
+    }
+
+    if (currentUrl === '/request') {
+      this.requestFlg = true;
+    }
   }
 
 }
