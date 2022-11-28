e4f830f790b7f0cebd586c64c751e274f2016a47
kamoto <takahiro.08039@gmail.com>
2022-11-28 23:14:40 +0900

    ホーム画面に追加する方法

--- src/app/component/page/add-home/add-home.component.ts
+++ src/app/component/page/add-home/add-home.component.ts
@@ -12,4 +12,49 @@ export class AddHomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * PC版の別タブ
+   */
+  public openPcWindow() {
+    const url = 'https://support.google.com/chrome/answer/188842?hl=ja&co=GENIE.Platform%3DDesktop&oco=1';
+
+    window.open(url);
+  }
+
+  /**
+   * iphone版のブックマーク保存用別タブ
+   */
+  public openIphoneBookmarkWindow() {
+    const url = 'https://support.google.com/chrome/answer/188842?hl=ja&co=GENIE.Platform%3DiOS&oco=1';
+
+    window.open(url);
+  }
+
+  /**
+   * iphone版のホーム画面追加用別タブ
+   */
+  public openIphoneAddHomeWindow() {
+    const url = 'https://www.softbank.jp/support/faq/view/12288';
+
+    window.open(url);
+  }
+
+  /**
+   * Android版の別タブ
+   */
+  public openAndroidBookmarkWindow() {
+    const url = 'https://support.google.com/chrome/answer/188842?hl=ja&co=GENIE.Platform%3DAndroid&oco=1';
+
+    window.open(url);
+  }
+
+  /**
+   * Android版の別タブ
+   */
+  public openAndroidAddHomeWindow() {
+    const url = 'https://support.google.com/chrome/answer/188842?hl=ja&co=GENIE.Platform%3DAndroid&oco=1';
+
+    window.open(url);
+  }
+
 }
